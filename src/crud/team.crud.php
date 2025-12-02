c01335b8928f29bb47a829c841c12e4d62e420c2
outoine15 <outoine15@gmail.com>
2025-12-02 14:37:48 +0100

    team crud partial

---
+++ src/crud/team.crud.php
@@ -0,0 +1,25 @@
+<?php 
+include_once "utility.crud.php";
+
+function select_team_by_id($conn, $id) {
+    $sql = "SELECT * FROM `Team` WHERE id = $id"; 
+    
+    global $debeug;
+    if ($debeug) echo $sql . "<br>"; 
+    
+    $res = mysqli_query($conn, $sql); 
+    $tab = rs_to_tab($res);
+    
+    return $tab[0];
+}
+
+function select_learners_by_team($conn,$teamId){
+    $sql = "SELECT * FROM `Learner` WHERE teamId= $teamId";
+
+    global $debeug;
+    if ($debeug) echo $sql . "<br>";
+
+    $res = mysqli_query($conn,$sql);
+    return rs_to_tab($res);
+}
+?>
