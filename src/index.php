70ff766bbb595ff5a075bb7924528cccd041496a
outoine15 <outoine15@gmail.com>
2025-12-09 15:41:07 +0100

    update learner:id

--- src/index.php
+++ src/index.php
@@ -12,7 +12,75 @@ $conn = db();
 
 get("/learners/:learnerId", function ($param) {
     $learner_id = $param['learnerId'];
-    echo $learner_id;
+    global $conn;
+    $res=select_activity($conn,$learner_id);
+    $id=$res["id"];
+    $firstName=$res["firstName"];
+    $lastName=$res["lastName"];
+    $email=$res["email"];
+    $pwd=$res["password"];
+    $stateId=$res["stateId"];
+    $teamId=$res["teamId"];
+
+    $resState=select_states($conn,$stateId);
+    $resSkill=select_skills_by_learner_id($conn,$id);
+    $resMark = select_marks_by_learner_id($conn,$id);
+
+    $stateId=$resState["id"];
+    $stateTitle=$resState["title"];
+    $stateColor=$resState["color"];
+    $stateIcon=$resState["icon"];
+    
+    $return = [
+        "id" => $id,
+        "firstName"=>$firstName,
+        "lastName"=>$lastName,
+        "email"=>$email,
+        "team"=>$teamId,
+        "state"=>[
+            "id"=>$stateId,
+            "title"=>$stateTitle,
+            "color"=>$stateColor,
+            "icon"=>$stateIcon,
+        ],
+        "skills"=>[],
+        "marks"=>[]
+    ];
+
+    $skillArray=[];
+    for ($i=0; $i < count($resSkill); $i++) { 
+
+        $name=$resSkill[$i]["name"];
+        $level=$resSkill[$i]["level"];
+        $color=$resSkill[$i]["color"];
+        $icon=$resSkill[$i]["icon"];
+
+        $curentSkill=[
+            "name"=>$name,
+            "level"=>$level,
+            "color"=>$color,
+            "icon"=>$icon
+        ];
+
+        array_push($skillArray,json_encode($curentSkill));
+    }
+    $return['skills'] = $skillArray;
+
+    $markArray=[];
+    for ($i=0; $i < count($resMark); $i++) {
+        $activityId= $resMark[$i]["activityId"];
+        $mark= $resMark[$i]["mark"];
+
+        $curentMark=[
+            "activityId"=>$activityId,
+            "mark"=>$mark
+        ];
+
+        array_push($markArray,json_encode($curentMark));
+    }
+    $return['marks']=$markArray;
+
+    echo json_encode($return);
     exit;
 
 });
@@ -92,42 +160,6 @@ post("/learners",function(){
         array_push($markArray,json_encode($curentMark));
     }
     $return['marks']=$markArray;
-
-        // $myObj = new stdClass();
-        // $myObj->name = "John";
-        // $myObj->age = 30;
-        // $myObj->city = "New York";
-        // 
-        // $myJSON = json_encode($myObj);
-        // 
-        // echo $myJSON;
-            // {
-            //   "id": 10,
-            //   "firstName": "theUser",
-            //   "lastName": "John",
-            //   "email": "john@email.com",
-            //   "team": 1,
-            //   "state": {
-                // "id": 1,
-                // "title": "En ligne",
-                // "color": "green",
-                // "icon": "check"
-            //   },
-            //   "skills": [
-                // {
-                //   "name": "agile",
-                //   "level": 2,
-                //   "color": "brown",
-                //   "icon": "brightness_1"
-                // }
-            //   ],
-            //   "marks": [
-                // {
-                //   "activityId": 1,
-                //   "mark": 5
-                // }
-            //   ]
-            // }
 
     echo json_encode($return);
     exit;
